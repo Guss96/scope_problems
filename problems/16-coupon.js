c2b714a40bec8fdb2d9a269a1d855099fc84268f
ghassen <amrighassen14@gmail.com>
2023-11-19 17:35:47 +0300

    coupon problem 16

--- problems/16-coupon.js
+++ problems/16-coupon.js
@@ -15,9 +15,13 @@ console.log(twentyPercent([10, 20, 30])); // [ 8, 16, 24 ]
 
 ***********************************************************************/
 function coupon(discount) {
-  // Your code here
+  return function newPrices (prices) {
+    return prices.map((el) =>
+     el - el * discount)
+      }
 }
-
+let tenPercent = coupon(0.1);
+console.log(tenPercent([10, 20, 30])); // [ 9, 18, 27 ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = coupon;
