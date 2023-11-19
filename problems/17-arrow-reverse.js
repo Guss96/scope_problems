46046fe10f54284cd14013ab05b343f87de3aae5
ghassen <amrighassen14@gmail.com>
2023-11-19 17:43:04 +0300

    arrow reverse prob 17

--- problems/17-arrow-reverse.js
+++ problems/17-arrow-reverse.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Write a function reverseStr(str) that accepts a string and returns that string
-reversed. 
+reversed.
 
 Write this function using a fat arrow function!
 
@@ -11,9 +11,17 @@ let result3 = reverseStr("potato"); // returns "otatop"
 
 
 ***********************************************************************/
-
-// Your code here
-
+const reverseStr = str => {
+  let reversed = "";
+  for (i = str.length-1; i >= 0; i--){
+    reversed += str[i];
+  }
+  return reversed;
+}
+let result1 = reverseStr("hello"); // returns "olleh"
+console.log(result1)
+let result2 = reverseStr("garden"); // returns "nedrag"
+let result3 = reverseStr("potato"); // returns "otatop"
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
     module.exports = reverseStr;
