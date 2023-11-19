7ab639f5a8e45bad958649a194f8c75531a2adf0
ghassen <amrighassen14@gmail.com>
2023-11-19 17:16:03 +0300

    smoothie problem 14

--- problems/14-smoothie-machine.js
+++ problems/14-smoothie-machine.js
@@ -4,7 +4,7 @@ and returns a function.
 
 The returned function will also accept any number of parameters and will
 return a string including all of the parameters of smoothieMachine as well
-as the returned function's parameters. Look at the examples for a guide of 
+as the returned function's parameters. Look at the examples for a guide of
 how your return should be formatted!
 
 See below for examples:
@@ -24,9 +24,27 @@ console.log(smoothie2("pineapple"));
 ***********************************************************************/
 
 const smoothieMachine = (...ingredients) => {
-  // Your code here
+  let arr = [];
+  return  function smoothie (...elements){
+    for (i = 0; i < elements.length; i++){
+    arr.push(elements[i]);
+  }
+    if (ingredients.length === 0){
+          return `I'm having a smoothie with ${arr.join(' and ')}`
+      } else {
+          return `I'm having a smoothie with ${ingredients.join(' and ')} and ${arr.join(' and ')}`
+      }
+  }
 };
 
+let smoothie1 = smoothieMachine();
+
+console.log(smoothie1("milk"));
+// prints "I'm having a smoothie with milk"
+console.log(smoothie1("kale", "spinach"));
+// prints "I'm having a smoothie with milk and kale and spinach"
+console.log(smoothie1("honey", "pears", "berries"));
+// prints "I'm having a smoothie with milk and kale and spinach and honey and pears and berries"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = smoothieMachine;
